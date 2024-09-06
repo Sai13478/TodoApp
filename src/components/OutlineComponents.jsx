159c781fa1928aab3043f8e5133e225cad1ffae9
V Sai Pramodu <saipramodvedantam@gmail.com>
2024-09-07 00:36:47 +0530

    first commit

---
+++ src/components/OutlineComponents.jsx
@@ -0,0 +1,9 @@
+import styles from "./outlinecomponent.module.css";
+
+export default function OutlineComponents() {
+  return (
+    <div>
+      <h1 className={styles.header}>Ths a outline components</h1>
+    </div>
+  );
+}
